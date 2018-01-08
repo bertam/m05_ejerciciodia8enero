e2cca93490edf1f7a02710f8ddf5294157129861
bertam <bertamerelo@gmail.com>
2018-01-08 19:45:27 +0100

    Añadidos ejes de circulo y rectangulo y creados los constructores de las clases

--- src/com/bertiwi/Circulo.java
+++ src/com/bertiwi/Circulo.java
@@ -1,8 +1,15 @@
 package com.bertiwi;
 
 public class Circulo {
-    private float radio;
+    private double radio;
+    private double ejeX;
+    private double ejeY;
 
+    public Circulo(double radio, double ejeX, double ejeY) {
+        this.radio = radio;
+        this.ejeX = ejeX;
+        this.ejeY = ejeY;
+    }
     public static void area(){}
     public static void perimetro(){}
 }
--- src/com/bertiwi/Rectangulo.java
+++ src/com/bertiwi/Rectangulo.java
@@ -3,7 +3,15 @@ package com.bertiwi;
 public class Rectangulo {
     private int base;
     private int altura;
+    private double ejeX;
+    private double ejeY;
 
+    public Rectangulo(int base, int altura, double ejeX, double ejeY) {
+        this.base = base;
+        this.altura = altura;
+        this.ejeX = ejeX;
+        this.ejeY = ejeY;
+    }
     public static void area(){}
     public static void perimetro(){}
 }
